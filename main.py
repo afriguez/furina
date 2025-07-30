71e7c2f641d5dce3f6aeedd8c6706a43dbe9794a
Fer L. <afriguez@gmail.com>
2025-07-30 07:52:25 -0500

    feat: add a reflection loop for companions

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import asyncio
 import signal
 import sys
 
+from app.companion.companion import Companion
 from app.companion.context_provider import get_context
 from app.loaders.entrypoint_loader import load_entrypoints
 
 terminate = False
+reflect_interval = 5
+
 def kill(sig, frame):
     global terminate
     print("\nShutting down... Please wait.")
@@ -14,19 +17,34 @@ def kill(sig, frame):
 signal.signal(signal.SIGINT, kill)
 signal.signal(signal.SIGTERM, kill)
 
+async def run_reflection_loop(companion: Companion):
+    try:
+        while not terminate:
+            await companion.reflect()
+            await asyncio.sleep(reflect_interval)
+    except asyncio.CancelledError:
+        pass
+
 async def main():
-    ctx = get_context()
+    companions, ctx = get_context()
     tasks = await load_entrypoints(ctx)
+    print(tasks)
+
+    reflection_tasks = [
+        asyncio.create_task(run_reflection_loop(companions[c]))
+        for c in companions
+    ]
+    tasks.extend(reflection_tasks)
+
     try:
         while not terminate:
             await asyncio.sleep(0.1)
     finally:
         for task in tasks:
-            if hasattr(task, "close"):
-                task.close()
-                await task.wait_closed()
-            elif hasattr(task, "cancel"):
-                task.cancel()
+            task.cancel()
+
+        await asyncio.gather(*tasks, return_exceptions=True)
+
         sys.exit(0)
 
 asyncio.run(main())
